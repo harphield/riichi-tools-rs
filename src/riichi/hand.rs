9484d1590cbb9ebe406161c9ca9d053c0ae312c4
harphield <harphield@gmail.com>
2021-01-12 22:25:19 +0100

    Red 5 parsing - kans

--- src/riichi/hand.rs
+++ src/riichi/hand.rs
@@ -504,9 +504,9 @@ impl Hand {
         let mut tiles: Vec<Option<Tile>> = Vec::new();
 
         //type
-        let t = kan.chars().next().unwrap();
+        let kan_type = kan.chars().next().unwrap();
         // number
-        let n = kan.chars().nth(1).unwrap();
+        let mut n = kan.chars().nth(1).unwrap();
         // color
         let c = kan.chars().nth(2).unwrap();
         // player
@@ -514,27 +514,54 @@ impl Hand {
             None => 0,
             Some(value) => value.to_digit(10).unwrap() as u8,
         } as u8;
+        // red 5 called?
+        let r = match kan.chars().nth(4) {
+            None => false,
+            Some(red) => red == 'r',
+        };
 
-        let mut tile = Tile::from_text(&format!("{}{}", n, c)[..]).unwrap();
+        if n != '0' && r {
+            return Err(RiichiError::new(589, "Only 0 can have r"));
+        }
 
-        tiles.push(Some(tile));
-        tiles.push(Some(tile));
-        tiles.push(Some(tile));
+        let mut first_tile = Tile::from_text(&format!("{}{}", n, c)[..]).unwrap();
+
+        if n == '0' {
+            // red 5, change to 5 for the other tiles
+            n = '5';
+        }
+
+        let mut second_tile = Tile::from_text(&format!("{}{}", n, c)[..]).unwrap();
+        let third_tile = Tile::from_text(&format!("{}{}", n, c)[..]).unwrap();
+        let fourth_tile = Tile::from_text(&format!("{}{}", n, c)[..]).unwrap();
 
         let shape = if p > 0 {
-            tile.called_from = p;
-            let open_kan_type = if t == 's' {
-                OpenKan::Shouminkan([tile, tile, tile, tile])
+            if r {
+                first_tile.called_from = p;
             } else {
-                OpenKan::Daiminkan([tile, tile, tile, tile])
+                second_tile.called_from = p;
+            }
+
+            let open_kan_type = if kan_type == 's' {
+                OpenKan::Shouminkan([first_tile, second_tile, third_tile, fourth_tile])
+            } else {
+                OpenKan::Daiminkan([first_tile, second_tile, third_tile, fourth_tile])
             };
 
             CompleteShape::Open(OpenShape::Kan(open_kan_type))
         } else {
-            CompleteShape::Closed(ClosedShape::Kantsu([tile, tile, tile, tile]))
+            CompleteShape::Closed(ClosedShape::Kantsu([
+                first_tile,
+                second_tile,
+                third_tile,
+                fourth_tile,
+            ]))
         };
 
-        tiles.push(Some(tile));
+        tiles.push(Some(first_tile));
+        tiles.push(Some(second_tile));
+        tiles.push(Some(third_tile));
+        tiles.push(Some(fourth_tile));
 
         Ok((tiles, shape))
     }
@@ -1680,6 +1707,30 @@ mod tests {
         let hand = Hand::from_text("123m111p222s33z(p0m1)", false).unwrap();
 
         assert_eq!(hand.to_string(), "123m111p222s33z(p0m1)");
+
+        let mut reds = 0;
+        for tile in hand.get_tiles().iter().filter(|t| match t {
+            None => false,
+            Some(tile) => tile.get_value() == 5 || tile.get_value() == 0,
+        }) {
+            match tile {
+                None => {}
+                Some(t) => {
+                    if t.is_red {
+                        reds += 1;
+                    }
+                }
+            }
+        }
+
+        assert_eq!(reds, 1);
+    }
+
+    #[test]
+    fn parse_hand_with_red_5_kan() {
+        let hand = Hand::from_text("123m111p222s33z(k0m1)", false).unwrap();
+
+        assert_eq!(hand.to_string(), "123m111p222s33z(k0m1)");
 
         let mut reds = 0;
         for tile in hand.get_tiles().iter().filter(|t| match t {
